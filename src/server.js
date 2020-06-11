a6ae9869b3f18279d818ced3370824959cd9bf12
Eduardo <eduneves95@gmail.com>
2020-06-10 22:38:26 -0300

    Updated routes

---
+++ src/server.js
@@ -0,0 +1,26 @@
+const express = require("express")
+const server = express()
+
+//configurar pasta publica
+server.use(express.static("public"))
+
+//Utilizando template engine
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
+})
+
+//Configurar caminhos da minha aplicação
+//Página inicial
+
+server.get("/", (req, res) => {
+    res.sendFile(__dirname + "/views/index.html")
+})
+
+server.get("/create-point", (req, res) => {
+    res.sendFile(__dirname + "/views/create-point.html")
+})
+
+//Ligar o servidor
+server.listen(3000)
